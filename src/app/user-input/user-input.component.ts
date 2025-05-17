07eb7157cd55ee1b54f5d5602a28e0e925aeb17f
onurabdulaji <onurabdulaji@gmail.com>
2025-05-17 02:10:10 +0200

    Practice

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  constructor(private investmentService: InvestmentService) {}
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+    });
+    this.enteredAnnualInvestment.set('0');
+    this.enteredInitialInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
